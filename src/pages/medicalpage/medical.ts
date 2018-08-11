7efcd17be18dc5ed4597f46bff4361de4a70c55d
DESKTOP-GLTN0TA\Vipin <vipinnanoboat@gmail.com>
2018-08-11 21:00:13 +0530

    AddedmedicalAlert

--- src/pages/medicalpage/medical.ts
+++ src/pages/medicalpage/medical.ts
@@ -1,9 +1,9 @@
 
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SplashPage } from '../splash/splash';
 import { MedicalService } from '../../app/medical.service';
-
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-medical',
   templateUrl: 'medical.html',
@@ -11,8 +11,50 @@ import { MedicalService } from '../../app/medical.service';
 })
 export class MedicalPage {
   medicalList
-  constructor(public navCtrl: NavController,private medicalSvc :MedicalService) {
-    this.medicalSvc.getMedicals().subscribe(data => this.medicalList = data);;
+  constructor(public navCtrl: NavController,private medicalSvc :MedicalService,public alertCtrl: AlertController) {
+      }
+      ngOnInit() {
+        this.medicalSvc.getMedicals().subscribe(data => this.medicalList = data);;
+  }
+
+   openDialog () {
+    const alert = this.alertCtrl.create({
+       buttons : [
+     {
+      
+       text: "AuthenticationPatient",
+       handler: data => {
+         
+          
+          
+         console.log("AuthenticationPatient clicked");
+       }
+     },
+     {
+       text: "Medicin Billing",
+       handler: data => {
+         console.log("Medicin Billing clicked");
+       }
+     },
+      {
+       
+       text: "Orders",
+       handler: data => {
+         console.log("History clicked");
+       }
+     },
+      {
+       
+       text: "Reports",
+       handler: data => {
+         console.log("Reports clicked");
+       }
+     }
+
+    ]      
+      
+    });
+    alert.present();
   }
   goSplash(){
     this.navCtrl.push(SplashPage);
