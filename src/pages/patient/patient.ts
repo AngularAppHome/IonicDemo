f99a447379c38a0edece615d40c3a4aea8034310
vijay jasti <vijay.jasti2010@gmail.com>
2018-08-11 20:13:41 +0530

    popupadded

--- src/pages/patient/patient.ts
+++ src/pages/patient/patient.ts
@@ -3,6 +3,9 @@ import { Component,OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SplashPage } from '../splash/splash';
 import { PatientService } from '../../app/patient.service';
+import { ActionSheetController } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-patient',
@@ -11,15 +14,53 @@ import { PatientService } from '../../app/patient.service';
 })
 export class PatientPage  implements OnInit {
   patientList
-  constructor(public navCtrl: NavController,private patientSvc :PatientService) {
+  constructor(public navCtrl: NavController,private patientSvc :PatientService,public actionSheetCtrl: ActionSheetController) {
     
   }
   ngOnInit() {
     //this.doctors=[{name:"Yogesh"},{name:"Gopi"},{name:"Seshu"}]
     this.patientSvc.getPatients().subscribe(data => this.patientList = data);;
   }
+  opendialog() {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Authentication Patient',
+          role: 'Authentication Patient',
+          handler: () => {
+            console.log('Authentication Patient clicked');
+          }
+        },{
+          text: 'Medical Billing',
+          role: 'Medical Billing',
+          handler: () => {
+            console.log('Medical Billing clicked');
+          }
+        },{
+          text: 'History',
+          role: 'History',
+          handler: () => {
+            console.log('History clicked');
+          }
+        },{
+          text: 'Reports',
+          role: 'Reports',
+          handler: () => {
+            console.log('Reports clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  
   goSplash(){
     this.navCtrl.push(SplashPage);
   }
-
 }
