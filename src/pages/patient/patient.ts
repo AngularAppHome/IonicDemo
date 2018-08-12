e28d2b27e7a041a3760435b61572c51fecc58dbe
vijay jasti <vijay.jasti2010@gmail.com>
2018-08-12 18:47:24 +0530

    sidebar addaed

--- src/pages/patient/patient.ts
+++ src/pages/patient/patient.ts
@@ -1,9 +1,9 @@
-
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SplashPage } from '../splash/splash';
 import { PatientService } from '../../app/patient.service';
 import { AlertController } from 'ionic-angular';
+import {MenuController} from 'ionic-angular';
 
 
 
@@ -14,12 +14,18 @@ import { AlertController } from 'ionic-angular';
 })
 export class PatientPage implements OnInit {
   patientList
-  constructor(public navCtrl: NavController, private patientSvc: PatientService, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, private patientSvc: PatientService, public alertCtrl: AlertController,public menuCtrl : MenuController) {
 
   }
   ngOnInit() {
     //this.doctors=[{name:"Yogesh"},{name:"Gopi"},{name:"Seshu"}]
     this.patientSvc.getPatients().subscribe(data => this.patientList = data);;
+  }
+  openPage(){
+    this.menuCtrl.open();
+  }
+  closeMenu(){
+    this.menuCtrl.close();
   }
   opendialog() {
     const prompt = this.alertCtrl.create({
